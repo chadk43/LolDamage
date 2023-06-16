0dce0bc82b32d4de2b9a6289a5f24d92681da28d
chadk43 <ckoivuneva43@gmail.com>
2023-06-16 17:17:07 -0600

    add starting files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+style={
+  backgroundColor:  #010A13;
+  
+}
+
+function App() {
+  return (
+    <div className="app-background">
+   
+    </div>
+  );
+}
+
+export default App;
